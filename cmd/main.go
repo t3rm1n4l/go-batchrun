1438bff01154254c166c47412a96e6686b54f3e2
Sarath Lakshman <sarathlakshman@slynux.com>
2013-12-12 02:39:22 +0530

    Implement batchrun
    Supply list of programs to be executed as arguments
    with concurrency and logdirectory

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,55 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"github.com/t3rm1n4l/go-batchrun"
 	"os"
 	"os/exec"
+	"path"
 )
 
-func CommandFn(name, cmd string) error {
-	out, _ := exec.Command(cmd).CombinedOutput()
-	log, _ := os.Create(name)
+func CommandFn(cmd, logfile string) error {
+	out, err := exec.Command(cmd).CombinedOutput()
+	log, _ := os.Create(logfile)
 	log.Write(out)
 	log.Close()
-	return nil
+
+	return err
 }
 
 func main() {
+	var (
+		logdir  = flag.String("logdir", ".", "Log directory")
+		concurr = flag.Int("concurrency", 1, "Concurrency")
+		help    = flag.Bool("help", false, "Help")
+	)
 
-	commands := map[string]string{
-		"ls":     "ls",
-		"whoami": "whoami",
+	var Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage %s: prog1 prog2 prog3...\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if flag.NArg() < 1 || *help {
+		Usage()
+		os.Exit(1)
 	}
 
 	runner := batch.New()
-	runner.SetConcurrency(3)
-	for name, cmd := range commands {
-		n := name
-		c := cmd
+	runner.SetConcurrency(*concurr)
+	for i := 0; i < flag.NArg(); i++ {
+		cmd := flag.Arg(i)
+		name := path.Base(cmd)
+		name2 := fmt.Sprintf("%s.%d", name, i)
+		logfile := path.Join(*logdir, fmt.Sprintf("%s.log", name2))
 		fn := func() {
+			c := cmd
+			n := name2
+			l := logfile
 			fmt.Println("Starting task : ", n)
-			CommandFn(n, c)
+			CommandFn(c, l)
 			fmt.Println("Completed task : ", n)
 		}
 
